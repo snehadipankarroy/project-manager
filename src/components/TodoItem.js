79fb4de9b5f28900ee193030120cff7043d51a7e
Sneha Dipankar Roy <snehadipankarroy@gmail.com>
2017-06-05 09:58:32 -0400

    Add files via upload

---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <li className="Todo">
+        {this.props.todo.title}
+      </li>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: React.PropTypes.object
+}
+
+export default TodoItem;
